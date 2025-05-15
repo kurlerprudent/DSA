eec9dde7fe68f117fce1f9c04f09c6870cd733c4
Prudentkurler <sarkodiekurler@gmail.com>
2025-05-15 06:09:31 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,15 @@
+// let str: string = 'Hello';
+// let str2: string = 'Obed';
+// let str3: string = '3';
+// let mySelf: string = `
+// ${str} everyone, my name is 
+// ${str2}, and I'm ${str3} years old`
+// let num1: number = parseInt(str3)
+// let combinedResult: string = str + str2;
+// console.log(combinedResult)
+// console.log(num1)
+// console.log(mySelf)
+var fruit = "Apple";
+for (var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
---
+++ strings.ts
@@ -0,0 +1,22 @@
+// let str: string = 'Hello';
+// let str2: string = 'Obed';
+// let str3: string = '3';
+
+// let mySelf: string = `
+// ${str} everyone, my name is 
+// ${str2}, and I'm ${str3} years old`
+
+// let num1: number = parseInt(str3)
+
+// let combinedResult: string = str + str2;
+
+
+// console.log(combinedResult)
+// console.log(num1)
+// console.log(mySelf)
+
+let fruit = "Apple"
+
+for(let i = 0; i < fruit.length; i++){
+    console.log(fruit[i])
+}
